8d09b7b61c664523043e12ddb4dfdf0a0b1ede9c
fakhryhizballah <falehry88@gmail.com>
2023-06-09 08:10:32 +0700

    layout

--- TA_2.ino
+++ TA_2.ino
@@ -1,311 +1,112 @@
-#include <IRrecv.h>
-#include <IRremoteESP8266.h>
-#include <IRutils.h>
- 
+#include <WiFi.h>
+#include "EspMQTTClient.h"
+#include "DHT.h"
+#include <ArduinoJson.h>
+#include <Arduino_JSON.h>
 
-const int RECV_PIN = 12;
-IRrecv irrecv(RECV_PIN);
-decode_results results;
-unsigned long key_value = 0;
+#define DHTPIN 4     // Digital pin connected to the DHT sensor
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+DHT dht(DHTPIN, DHTTYPE);
 
-int nilai_angka_tombol_pertama;//variabel untuk mendeklarasikan nilai awal untuk masing-masing tombol untuk memasukan perintah untuk nilai pertama
-int nilai_angka_tombol_kedua;//variabel untuk mendeklarasikan nilai awal untuk masing-masing tombol untuk memasukan perintah untuk nilai kedua
+const unsigned int TRIG_PIN = D0; // Pin untuk sensor jarak
+const unsigned int ECHO_PIN = D3; // Pin untuk sensor jarak
 
-String penjumlahan_angka_pertama = "";//variabel untuk menjumlahkan angka pertama
-String penjumlahan_angka_kedua = "";//variabel untuk menjumlahkan angka kedua
-int nilai_angka_penggabungan;//variabel untuk mendeklarasikan nilai awal untuk penggabungan antara nilai pertama dan nilai kedua
 
-int nilai_angka_pertama = 0;//variabel untuk mendeklarasikan nilai awal untuk angka pertama
-int nilai_angka_kedua = 0;//variabel untuk mendeklarasikan nilai awal untuk angka kedua
+EspMQTTClient client(
+  "WifiSSID",    // Wifi SSID
+  "WifiPassword",   // Wifi password
+  "test.mosquitto.org",  // MQTT Broker server ip
+  // "MQTTUsername",   // Can be omitted if not needed
+  // "MQTTPassword",   // Can be omitted if not needed
+  "TestClient"      // Client name that uniquely identify your device
+);
 
-int pengunci_angka_pertama = 0;//variabel untuk mengunci perintah "masukan angka pertama" agar perintah tersebut dapat terus berjalan sampai pengguna menonaktifkannya sendiri
-int pengunci_angka_kedua = 0;//variabel untuk mengunci perintah "masukan angka kedua" agar perintah tersebut dapat terus berjalan sampai pengguna menonaktifkannya sendiri
 
 void setup() {
-  Serial.begin(115200);
-  irrecv.enableIRIn();
+   Serial.begin(115200);
+   dht.begin();
+
+   pinMode(TRIG_PIN, OUTPUT);
+   pinMode(ECHO_PIN, INPUT);
 }
 
 void loop() {
-  if (irrecv.decode(&results)) {//mengaktifkan remote
-    if (results.value == 0XFFFFFFFF)
-      results.value = key_value;
-    switch (results.value) {
-      case 0xFFA25D://tombol 1
-        delay(100);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 1;//
-        break;
+  client.loop();
+}
 
-      case 0xFF629D://tombol 2
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 2;
-        break;
+void onConnectionEstablished() {
 
-      case 0xFFE21D://tombol 3
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 3;
-        break;
+  client.subscribe("mytopic/test", [] (const String &payload)  {
+    Serial.println(payload);
+    StaticJsonDocument<192> doc;
 
-      case 0xFF22DD://tombol 4
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 4;
-        break;
+    DeserializationError error = deserializeJson(doc, input);
 
-      case 0xFF02FD://tombol 5
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 5;
-        break;
+if (error) {
+  Serial.print("deserializeJson() failed: ");
+  Serial.println(error.c_str());
+  return;
+}
 
-      case 0xFFC23D://tombol 6
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 6;
-        break;
-
-      case 0xFFE01F://tombol 7
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 7;
-        break;
-
-      case 0xFFA857://tombol 8
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 8;
-        break;
-
-      case 0xFF906F://tombol 9
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 9;
-        break;
-
-      case 0xFF6897://tombol bintang(*)
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 10;
-        break;
-
-      case 0xFF9867://tombol 0
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 0;
-        break;
-
-      case 0xFFB04F://tombol pagar(#)
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 12;
-        break;
-
-      case 0xFF18E7://tombol atas
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        nilai_angka_tombol_pertama = 13;
-        break;
-
-      case 0xFF10EF://tombol kiri
-        delay(200);//MEMBERIKAN JEDA WAKTU PADA SAAT SETELAH MENEKAN TOMBOL AGAR TIDAK TERJADI PENGULANGAN MELAKUKAN INPUT
-        Serial.println("masukkan angka pertama!!!");
-        nilai_angka_tombol_pertama = 14;
-        break;
-
-      case 0xFF38C7://tombol ok
-        delay(200);
-        nilai_angka_tombol_pertama = 15;
-        break;
-
-      case 0xFF5AA5://tombol kanan
-        delay(200);
-        Serial.println("masukkan angka kedua!!!");
-        nilai_angka_tombol_pertama = 16;
-        break;
-
-      case 0xFF4AB5://tombol bawah
-        delay(200);
-        nilai_angka_tombol_pertama = 17;
-        break;
+const char* jenis = doc["jenis"]; // "Kubus"
+int tonase = doc["tonase"]; // 1234
+int a, b, h, l;
+double hx ;
+if(jenis = "Kubus"){
+  a= doc["a"]; // Panjang
+  b= doc["b"]; // Lebar
+  h= doc["h"]; // Tinggi
+}
+if (jenis = "Trapezoidal"){
+  a= doc["a"]; // Sisi Atas
+  b= doc["b"]; // sisi Bawah
+  h= doc["h"]; // Tinggi max
+  l= doc["l"]; // lebar
     }
-    key_value = results.value;
-    irrecv.resume();
+  });
+
+  
+}
+
+double volumeKubus(double a, double b, double h, double hx) {
+  // Serial.println(a*b*c);
+  return;
+}
+
+double volumeTrapezoidal(double a, double b, double h, double l, double hx) {
+  // Serial.println((a+b)/2*h);
+  return;
+}
+
+double Temp(){
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float t = dht.readTemperature();
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
   }
-  ///////////////////////////////////////////////////////////////BAGIAN UNTUK MEMBUAT ANGKA PERTAMA///////////////////////////////////////////////////////////////
-  if (nilai_angka_tombol_pertama == 14) {
-    pengunci_angka_pertama = 1;
-  }
-  if (pengunci_angka_pertama == 1) {
-    if (nilai_angka_tombol_pertama == 1) {
-      nilai_angka_pertama = 1;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 2) {
-      nilai_angka_pertama = 2;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 3) {
-      nilai_angka_pertama = 3;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 4) { 
-      nilai_angka_pertama = 4;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 5) {
-      nilai_angka_pertama = 5;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 6) {
-      nilai_angka_pertama = 6;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 7) {
-      nilai_angka_pertama = 7;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 8) {
-      nilai_angka_pertama = 8;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 9) {
-      nilai_angka_pertama = 9;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 0) {
-      nilai_angka_pertama = 0;
-      penjumlahan_angka_pertama = String(penjumlahan_angka_pertama) + String(nilai_angka_pertama);
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 13;
-    }
-    if (nilai_angka_tombol_pertama == 10) {
-      penjumlahan_angka_pertama = "";
-      Serial.print("angka pertama: ");
-      Serial.println(penjumlahan_angka_pertama); //MENAMPILKAN NILAI ANGKA PERTAMA
-      nilai_angka_tombol_pertama = 14;
-    }
-    if (nilai_angka_tombol_pertama == 12) {
-      Serial.println("input angka pertama telah selesai");
-      pengunci_angka_pertama = 0;
-    }
-  }
-  ///////////////////////////////////////////////////////////////BAGIAN UNTUK MEMBUAT ANGKA PERTAMA///////////////////////////////////////////////////////////////
+  return t;
+}
 
-  ////////////////////////////////////////////////////////////////BAGIAN UNTUK MEMBUAT ANGKA KEDUA////////////////////////////////////////////////////////////////
-  if (nilai_angka_tombol_pertama == 16) {
-    pengunci_angka_kedua = 1;
-  }
-  if (pengunci_angka_kedua == 1) {
-    if (nilai_angka_tombol_pertama == 1) {
-      nilai_angka_kedua = 1;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 2) {
-      nilai_angka_kedua = 2;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 3) {
-      nilai_angka_kedua = 3;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 4) {
-      nilai_angka_kedua = 4;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 5) {
-      nilai_angka_kedua = 5;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 6) {
-      nilai_angka_kedua = 6;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 7) {
-      nilai_angka_kedua = 7;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 8) {
-      nilai_angka_kedua = 8;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 9) {
-      nilai_angka_kedua = 9;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 0) {
-      nilai_angka_kedua = 0;
-      penjumlahan_angka_kedua = String(penjumlahan_angka_kedua) + String(nilai_angka_kedua);
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 10) {//PERINTAH MELAKUKAN DELETE
-      penjumlahan_angka_kedua = "";
-      Serial.print("angka kedua: ");
-      Serial.println(penjumlahan_angka_kedua); //MENAMPILKAN NILAI ANGKA KEDUA
-      nilai_angka_tombol_pertama = 16;
-    }
-    if (nilai_angka_tombol_pertama == 12) {
-      Serial.println("input angka kedua telah selesai");
-      pengunci_angka_kedua = 0;
-    }
-  }
-  ////////////////////////////////////////////////////////////////BAGIAN UNTUK MEMBUAT ANGKA KEDUA////////////////////////////////////////////////////////////////
 
-  ////////////////////////////////////////////////BAGIAN UNTUK MEMBUAT MENGGABUNGKAN ANGKA PERTAMA DAN ANGKA KEDUA////////////////////////////////////////////////
-  if (nilai_angka_tombol_pertama == 15) {
-    nilai_angka_penggabungan = penjumlahan_angka_pertama.toInt() + penjumlahan_angka_kedua.toInt();//MENGUBAH VARIABEL PADA penjumlahan_angka_pertama DAN penjumlahan_angka_kedua MENJADI DATA INTEGER YANG SEBELUMNYA ADALAH DATA STRING DENGAN MENGGUNAKAN PERINTAH toInt
-    Serial.print("hasil: ");
-    Serial.println(nilai_angka_penggabungan); //MENAMPILKAN NILAI HASIL PENGGABUNGAN KEDUA NILAI
-    nilai_angka_tombol_pertama = 99;
-  }
-  if (nilai_angka_tombol_pertama == 12) {
-    nilai_angka_penggabungan = 0;//MERESET NILAI HASIL MENJADI 0 (NOL)
-  }
-  ////////////////////////////////////////////////BAGIAN UNTUK MEMBUAT MENGGABUNGKAN ANGKA PERTAMA DAN ANGKA KEDUA////////////////////////////////////////////////
+double mersure(double temp){
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Menghitung kecepatan suara berdasarkan suhu
+  double v = 331,4 + (0,6 x temp)
+
+  const unsigned long duration = pulseIn(ECHO_PIN, HIGH);
+    // Calculating the distance
+  // distance = duration * 0.034 / 2;
+  double distance = duration / v / 2;
+  return distance;
 }
